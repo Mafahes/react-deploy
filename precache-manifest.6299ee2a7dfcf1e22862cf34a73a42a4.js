c735e205a43c03d954d2e49aeb032b6f997ae9da
Mafahes <v.rimov@mail.ru>
2020-05-01 23:52:59 +0300

    Updates

---
+++ precache-manifest.6299ee2a7dfcf1e22862cf34a73a42a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1c82586afe725c5a92287b080378bf",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "ec7ce1341bcd7093e324",
+    "url": "/react-deploy/static/css/main.30745640.chunk.css"
+  },
+  {
+    "revision": "7e20c3ce2b78407953b2",
+    "url": "/react-deploy/static/js/2.3f6c1102.chunk.js"
+  },
+  {
+    "revision": "effe9ac712adea963d93b446f3df3893",
+    "url": "/react-deploy/static/js/2.3f6c1102.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec7ce1341bcd7093e324",
+    "url": "/react-deploy/static/js/main.cd3aa40c.chunk.js"
+  },
+  {
+    "revision": "9fdd5d469e71733e5a6c",
+    "url": "/react-deploy/static/js/runtime-main.f7ab85b0.js"
+  },
+  {
+    "revision": "1480e4ea207e910657863c7c4f25d7c4",
+    "url": "/react-deploy/static/media/browser.1480e4ea.svg"
+  },
+  {
+    "revision": "1150b4ac6f86462f5a305f9da250c8a6",
+    "url": "/react-deploy/static/media/ranepa.1150b4ac.png"
+  },
+  {
+    "revision": "4e082a2a1e6d4a52f7ce491e76adf7cb",
+    "url": "/react-deploy/static/media/ranepa.4e082a2a.png"
+  }
+]);
